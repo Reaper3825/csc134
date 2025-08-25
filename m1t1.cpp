9243c262d4b45f3621e4eecc6633c2655447ed50
Austin Wadkins <166045987+Reaper3825@users.noreply.github.com>
2025-08-25 11:20:50 -0400

    --personalized greeting messsage

---
+++ m1t1.cpp
@@ -0,0 +1,20 @@
+/*
+m1t1: hello, user
+Wadkinsa, 8/25/25
+INPUT: run the program
+process: None Yet
+OUTPUT: return 0
+SPECIAL FX: Greet the user
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+// SFX: Greet user
+//strings hold words
+{ cout << "Hello, Austin!" << endl;
+    return 0;
+// output
+return 0; // no errors
+} 
