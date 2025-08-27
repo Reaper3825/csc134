a1f0204c6544b4dbf41e3c6d693d008bb7e28ab7
Austin Wadkins <166045987+Reaper3825@users.noreply.github.com>
2025-08-27 14:27:39 +0000

    --MM02

---
+++ m1lab.cpp
@@ -0,0 +1,17 @@
+/*
+csc 134
+m1lab
+wadkins
+8/27/2025
+purpose: The gem
+*/
+#include <iostream>
+
+using namespce std::string;
+
+//global var dec
+
+int main() {
+    cout << "hello world" << endl;
+    
+}
