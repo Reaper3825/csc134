3ac426520d0ade22cfacd9eb2d29973eb709407b
Austin Wadkins <166045987+Reaper3825@users.noreply.github.com>
2025-08-27 15:19:58 +0000

    --learning01

--- m1lab.cpp
+++ m1lab.cpp
@@ -7,17 +7,23 @@ First program.
 */
 // magic words
 #include <iostream>
+#include "m1lab.hpp"
 using namespace std;
 
 //GLobal Var declarations
 
 
 int main() {
-    string item_name = "gem";
+    string item_name = "gym";
+    int  num_items =10;
+    double cost_per = 0.99;
     cout<< "Hello, World!" << endl;
-    cout << "My foo is C++" << endl; //put your 'foo' here
-  cout << "Welcome to the " << item_name << "store" endl; //unreachable code
-
+    cout << "Welcome to the " << item_name << " store" << "\n";
+    cout << "We have " << num_items << " " << item_name << " total" << "\n";
+// do the processing
+    double total_cost = num_items * cost_per;
+    cout << "The total cost is $" << total_cost << "\n";
+    cout << "thank you for shopping!" << "\n";
     return 0;
 
   
---
+++ m1lab.hpp
@@ -0,0 +1,8 @@
+//m1lab.hpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, M1 Lab!" << endl;
+    return 0;
+}
