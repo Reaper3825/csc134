b1186ff074b8ee9dbd4213b7d6c80e8605c4fadb
Austin Wadkins <166045987+Reaper3825@users.noreply.github.com>
2025-08-27 14:34:09 +0000

    --mm04

--- m1lab.cpp
+++ m1lab.cpp
@@ -1,16 +1,17 @@
 /*
-csc 134
-m1lab
-wadkins
-8/27/2025
-purpose: The gem
+CSC 134
+M1T1 - Hello World
+A Wadkins
+8/20/25
+First program.
 */
+// magic words
 #include <iostream>
-
-using namespce std;
+using namespace std;
 
 int main() {
     cout<< "Hello, World!" << endl;
+    cout << "My foo is C++" << endl; //put your 'foo' here
     return 0;
 
 }
