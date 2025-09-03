8078c75e9fc7a5d262b8dcdaec68a42117e9566e
Austin Wadkins <166045987+Reaper3825@users.noreply.github.com>
2025-09-03 14:57:08 +0000

    --added user input output

--- m2t1.cpp
+++ m2t1.cpp
@@ -1,10 +1,10 @@
 //m2t1.cpp
-
-#inculude <iostream>
+#include <string>
+#include <iostream>
 using namespace std;
 
 int main() {
-------Declerations--------
+//Declerations
 string item = "apples";
 double cost_per = 0.99;
 int amount = 20;
@@ -13,11 +13,17 @@ int amount_puchased;
 double total_cost;
 
 //Greet the user
-cout << "Hell! Welcome to our " << item << " store." < end1;
+cout << "Hell! Welcome to our " << item << " store." << endl;
 cout << "Each of the " << item << " cost $" << cost_per << endl;
 cout << "We have " << amount << " for sale." << endl;
 cout<< endl;
 cout << "How many would you like to buy?" << endl;
+cin >> amount_puchased;
 
 
+total_cost = amount_puchased * cost_per;
+
+cout << "You are buying " << amount_puchased << " " << item << endl;
+cout << "Thank you for shopping with us." << endl;
+return 0;
 }
