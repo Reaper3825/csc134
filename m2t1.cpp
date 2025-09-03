553e73b5592b01fbd7ee4f7050eb937b81e97d98
Austin Wadkins <166045987+Reaper3825@users.noreply.github.com>
2025-09-03 10:45:03 -0400

    --added m2t1

---
+++ m2t1.cpp
@@ -0,0 +1,23 @@
+//m2t1.cpp
+
+#inculude <iostream>
+using namespace std;
+
+int main() {
+------Declerations--------
+string item = "apples";
+double cost_per = 0.99;
+int amount = 20;
+//UI variables
+int amount_puchased;
+double total_cost;
+
+//Greet the user
+cout << "Hell! Welcome to our " << item << " store." < end1;
+cout << "Each of the " << item << " cost $" << cost_per << endl;
+cout << "We have " << amount << " for sale." << endl;
+cout<< endl;
+cout << "How many would you like to buy?" << endl;
+
+
+}
