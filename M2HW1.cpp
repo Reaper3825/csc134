44247a8771dc2c4e831cb5b127d0a072038eeedf
Austin Wadkins <166045987+Reaper3825@users.noreply.github.com>
2025-09-15 15:14:55 +0000

    --completed m2t2

---
+++ M2HW1.cpp
@@ -0,0 +1,25 @@
+/*
+M2HW1: hello, user
+Wadkinsa, 8/25/25
+INPUT: run the program
+process: None Yet
+OUTPUT: return 0
+SPECIAL FX: Greet the user
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main() {
+
+
+
+cout << "Example of printing out correct money values" << endl;
+
+cout << setprecision(2) << endl;
+
+
+return 0;
+    //call each question 
+}
---
+++ m2t2.cpp
@@ -0,0 +1,24 @@
+//m2t1.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+//Declerations
+string item = "Burger";
+double pbt = 5.99;
+double tax_percent = 0.08;
+double tax_amount;
+double total_price;
+
+cout << "Order up" << endl;
+cout << item << " (x1)" << endl;
+
+//Greet the user
+cout << item << ":" << "      " << total_price << "$" << endl;
+cout << total_price << endl;
+
+
+
+return 0;
+}
