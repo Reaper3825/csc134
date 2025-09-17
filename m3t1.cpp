872558ac6e6f9dc6dbc83831d64f0dcac2f05575
Austin Wadkins <166045987+Reaper3825@users.noreply.github.com>
2025-09-17 15:04:09 +0000

    -- competed m3t1

---
+++ m3t1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+double length1, length2, width1, width2;
+double area1, area2;
+cout << "hello world" << endl; 
+cout <<"Enter the length and width of two retangles." << endl;
+cout << "First rectangle." << endl;
+cout << "Length: ";
+cin >> length1;
+
+cout << "Width: ";
+cin >> width1;
+cout << endl;
+
+cout << "Second rectangle." << endl;
+cout << "Length: ";
+
+cin >> length2;
+cout <<"Width: ";
+cin >> width2;
+
+area1 = length1 * width1;
+area2 = length2 * width2;
+
+cout << "First rectangle area = " << area1 << endl;
+cout << "Second rectangle area = " << area2 << endl;
+
+//compare tangles
+
+if (area1 > area2) {
+    cout << "The first rectangle is larger." << endl;
+}
+
+if (area2 > area1) {
+    cout << "The Second rectangle is larger." << endl;
+}
+
+if (area2 == area1) {
+    cout << "They are the same size!" << endl;
+}
+
+
+
+return 0;
+}
